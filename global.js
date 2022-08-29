0f59e207ca88064a0bcb09b96cc33b24c441fe2b
parsanoroozi <parsan6347@gmail.com>
2022-08-29 15:56:09 +0430

    first one

---
+++ global.js
@@ -0,0 +1,11 @@
+// setTimeout(() => {
+//   console.log("in the timeout ");
+//   clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log(Date.now());
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
---
+++ modules.js
@@ -0,0 +1,11 @@
+const xyz = require("./people");
+const { people, ages } = require("./people");
+
+// console.log(xyz.people, xyz.ages);
+
+// console.log("people", people);
+// console.log("ages", ages);
+
+const os = require("os");
+
+console.log(os.platform(), os.homedir());
